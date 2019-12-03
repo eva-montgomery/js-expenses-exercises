c6ada503cbca26e57e18d21b5f9e1be474113ecc
eva montgomery <eva.m.montgomery@gmail.com>
2019-12-03 14:51:51 -0500

    adds mapping and filter functions

--- scripts/index.js
+++ scripts/index.js
@@ -77,9 +77,9 @@ const discounted = [
   30.00
 ];
 
-function itemDiscount(array) {
+function itemDiscount(expenseArray) {
   let newArray = [];
-  for (let cost of array) {
+  for (let cost of expenseArray) {
     if (cost >= 100) {
       console.log(cost)
       cost = cost * 0.9;
@@ -94,3 +94,58 @@ function itemDiscount(array) {
   }
 
 console.log(itemDiscount(discounted));
+
+////////////////////////////
+
+function alwaysDiscounted(expenseArray) {
+  let newArray = [];
+
+  for (let cost of expenseArray) {
+    cost = cost * 0.9;
+    newArray.push(cost);
+  }
+
+  return newArray;
+}
+
+// How to transform one array into a corresponding array:
+
+// 1. Write a function that describes how to transform one, single item.
+function discountExpense(singleExpense) {
+  //debugger;
+  return singleExpense * 0.9;
+}
+
+// 2. Declare a variable to hold a new array:
+let updatedExpenses = expenses.map(discountExpense);
+
+///////// long version
+
+function onlyOverTwentyDollars(expenseArray) {
+  let newArray = [];
+
+  for (let cost of expenseArray) {
+    if (cost >= 20) {
+      newArray.push(cost);
+    }
+  }
+  return newArray;
+}
+let filteredExpenses = onlyOverTwentyDollars(expenses);
+
+// How to filter out certain items:
+
+// 1. Write the helper function that knows if one, single element should be allowed or not.
+// "Gandolf function"
+
+function isOverTwentyDollars(cost) {
+  //debugger;
+  if (cost >= 20) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// 2. Declare a variable to hold out new, filtered array.
+let highExpenses = expenses.filter(isOverTwentyDollars);
